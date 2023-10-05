6c172695545c8ccf01b84f4d9f2a0eb629915746
SonaHunanyan <126061903+sonaleoniehill@users.noreply.github.com>
2023-10-05 15:45:39 +0400

    REF code

---
+++ lib/data/constant/api_key.dart
@@ -0,0 +1,3 @@
+class ApiKey {
+  static const weather = 'c2cf3adcc65c876fbe91e5d7ec04f44c';
+}
--- lib/data/repository/weather_repository.dart
+++ lib/data/repository/weather_repository.dart
@@ -1,3 +1,4 @@
+import 'package:weather_test_task/data/constant/api_key.dart';
 import 'package:weather_test_task/data/constant/app_url.dart';
 import 'package:weather_test_task/data/http/dio.dart';
 import 'package:weather_test_task/domain/model/weather/weather.dart';
@@ -8,7 +9,7 @@ class WeatherRepository implements IWeatherRepository {
   Future<Weather> getWeather({required String country}) async {
     final params = <String, dynamic>{
       'q': country,
-      'appid': 'c2cf3adcc65c876fbe91e5d7ec04f44c',
+      'appid': ApiKey.weather,
     };
 
     final resp = await dio.get(
